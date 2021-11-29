fedcc5ae8e24ff5dcf324de2ca3e150c5df71f0f
Anthony Matteis <amatt9425@gmail.com>
2021-11-28 18:47:10 -0600

    Updated to net6

--- API/Program.cs
+++ API/Program.cs
@@ -1,50 +1,99 @@
-using System;
-using System.Threading.Tasks;
 using Core.Entities.Identity;
-using Infrastructure.Data;
-using Infrastructure.Identity;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
 
-namespace API
+var builder = WebApplication.CreateBuilder(args);
+
+// add services to the container
+
+builder.Services.AddAutoMapper(typeof(MappingProfiles));
+builder.Services.AddControllers();
+// services.AddDbContext<AppDBContext>(x =>
+//     x.UseSqlServer(_config.GetConnectionString("DefaultConnection")));
+// services.AddDbContext<AppDBContext>(x =>
+//     x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<AppDBContext>(x =>
+    x.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<AppIdentityDBContext>( x =>
+    x.UseNpgsql(builder.Configuration.GetConnectionString("IdentityConnection")));
+
+builder.Services.AddSingleton<IConnectionMultiplexer>(c => {
+    var configuration =
+        ConfigurationOptions.Parse(builder.Configuration.GetConnectionString("Redis"), true);
+    return ConnectionMultiplexer.Connect(configuration);    
+});
+
+builder.Services.AddApplicationServices();
+builder.Services.AddIdentityServices(builder.Configuration);
+builder.Services.AddSwaggerDocumentation();
+builder.Services.AddCors(opt =>
 {
-    public class Program
+    opt.AddPolicy("CorsPolicy", policy =>
     {
-        public static async Task Main(string[] args)
-        {
-            var host = CreateHostBuilder(args).Build();
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                var loggerFactory = services.GetRequiredService<ILoggerFactory>();
-                try
-                {
-                    var context = services.GetRequiredService<AppDBContext>();
-                    await context.Database.MigrateAsync();
-                    await AppDbContextSeed.SeedAsync(context, loggerFactory);
+        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+    });
+});
+   
+// configure the http request pipeline
 
-                    var userManager = services.GetRequiredService<UserManager<AppUser>>();
-                    var identityContext = services.GetRequiredService<AppIdentityDBContext>();
-                    await identityContext.Database.MigrateAsync();
-                    await AppIdentityDbContextSeed.SeedUsersAsync(userManager);
-                }
-                catch (Exception ex)
-                {
-                    var logger = loggerFactory.CreateLogger<Program>();
-                    logger.LogError(ex, "An error occured during migration");
-                }
-            }
-            host.Run();
-        }
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+var app = builder.Build();
+
+app.UseMiddleware<ExceptionMiddleware>();
+
+if (builder.Environment.IsDevelopment())
+{
+    app.UseSwaggerDocumentation();
+}
+
+app.UseStatusCodePagesWithReExecute("/errors/{0}");
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(Directory.GetCurrentDirectory(), "Content")
+    ), RequestPath = "/content"
+});
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+        Path.Combine(Directory.GetCurrentDirectory(), "Content")
+    ), RequestPath = "/content"
+});
+
+app.MapControllers();
+app.MapFallbackToController("Index", "Fallback");
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+    try
+    {
+        var context = services.GetRequiredService<AppDBContext>();
+        await context.Database.MigrateAsync();
+        await AppDbContextSeed.SeedAsync(context, loggerFactory);
+
+        var userManager = services.GetRequiredService<UserManager<AppUser>>();
+        var identityContext = services.GetRequiredService<AppIdentityDBContext>();
+        await identityContext.Database.MigrateAsync();
+        await AppIdentityDbContextSeed.SeedUsersAsync(userManager);
+    }
+    catch (Exception ex)
+    {
+        var logger = loggerFactory.CreateLogger<Program>();
+        logger.LogError(ex, "An error occured during migration");
     }
 }
+
+await app.RunAsync();
--- Core/Entities/Member.cs
+++ Core/Entities/Member.cs
@@ -8,16 +8,17 @@ namespace Core.Entities
         public Guid TKey { get; set; }
         public string FirstName { get; set; }
         public string MiddleInitial { get; set; }   
+
         public string LastName { get; set; }
-        public DateTime DateOfBirth { get; set; }
+        public DateTime DateOfBirth { get; set; } = DateTime.UtcNow;
         public string EMail { get; set; }
         public int DriversLicenseNumber { get; set; }
         public string Status { get; set; }
-        public DateTimeOffset DateCreated { get; set; } = DateTimeOffset.UtcNow;
+        public DateTime DateCreated { get; set; } = DateTime.UtcNow;
         public string LocalTimezoneCreated { get; set; } =
             TimeZoneInfo.Local.IsDaylightSavingTime(DateTime.UtcNow)
                 ? TimeZoneInfo.Local.DaylightName : TimeZoneInfo.Local.StandardName; 
-        public DateTimeOffset DateUpdated { get; set; } = DateTimeOffset.UtcNow;
+        public DateTime DateUpdated { get; set; } = DateTime.UtcNow;
         public string LocalTimezoneUpdated { get; set; }
     }
 }
